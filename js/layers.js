d2936e3f9cf2d40e16e514bb4e9896dbbd6d976b
ceaserinc <ytgbgaming@gmail.com>
2020-12-24 13:35:23 -0600

    s

--- js/layers.js
+++ js/layers.js
@@ -14,7 +14,7 @@ addLayer("T", {
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent(){
         if(getBuyableAmount(this.layer,11).gte(1)){
-            return 0.5/(1+(1/10*getBuyableAmoute(this.layer,11)))
+            return 0.5/(1+(1/10*getBuyableAmount(this.layer,11)))
         }
     }, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
